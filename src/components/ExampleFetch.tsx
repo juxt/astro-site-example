ce071f7433c6f9e9895eb2b5e1df3abf5fffb0cb
Lucio D'Alessandro <lda@juxt.pro>
2023-02-17 15:21:46 +0100

    Polish small bits

--- src/components/ExampleFetch.tsx
+++ src/components/ExampleFetch.tsx
@@ -1,27 +1,22 @@
 import { useEffect, useState } from 'preact/hooks'
 
 export function ExampleFetch() {
-  const [buttonClicked, setButtonClicked] = useState(false)
   const [fetchedData, setFetchedData] = useState<{
     results: { email: string }[]
   }>()
 
-  useEffect(() => {
-    const fetchFn = async () => {
-      const response = await fetch('https://randomuser.me/api/')
-      const data = await response.json()
-      console.log(data)
-      setFetchedData(data)
-    }
-
-    buttonClicked && fetchFn()
-  }, [buttonClicked])
+  const fetchFn = async () => {
+    const response = await fetch('https://randomuser.me/api/')
+    const data = await response.json()
+    console.log(data)
+    setFetchedData(data)
+  }
 
   return (
     <div className={'py-4'}>
       <button
         className={'border-2 border-white rounded-md p-2 uppercase'}
-        onClick={() => setButtonClicked(true)}
+        onClick={() => fetchFn()}
       >
         Fetch
       </button>
